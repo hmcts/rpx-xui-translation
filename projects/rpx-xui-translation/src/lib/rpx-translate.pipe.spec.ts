ae0b4ee2d209a76b75bdaeb5039b6b470c85462d
Matei Baciu <matei@baciu.co.uk>
2023-05-04 15:25:04 +0100

    tests

--- projects/rpx-xui-translation/src/lib/rpx-translate.pipe.spec.ts
+++ projects/rpx-xui-translation/src/lib/rpx-translate.pipe.spec.ts
@@ -1,8 +1,78 @@
 import { RpxTranslatePipe } from './rpx-translate.pipe';
+import { RpxTranslationService } from './rpx-translation.service';
+import { ChangeDetectorRef } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('RpxTranslatePipe', () => {
+  let translationServiceMock: jasmine.SpyObj<RpxTranslationService>;
+  let changeDetectorRefMock: jasmine.SpyObj<ChangeDetectorRef>;
+  let pipe: RpxTranslatePipe;
+
+  beforeEach(() => {
+    translationServiceMock = jasmine
+      .createSpyObj('RpxTranslationService', ['getTranslationWithReplacements', 'getYesTranslationReplacement', 'getNoTranslationReplacement', 'getTranslation']);
+    changeDetectorRefMock = jasmine.createSpyObj('ChangeDetectorRef', ['markForCheck']);
+    pipe = new RpxTranslatePipe(translationServiceMock, changeDetectorRefMock);
+  });
+
   it('create an instance', () => {
-    const pipe = new RpxTranslatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  describe('transform', () => {
+    it('should return null when called with null or undefined', () => {
+      expect(pipe.transform(null)).toBeNull();
+      expect(pipe.transform(undefined)).toBeNull();
+    });
+
+    it('should return the same string when called with a string', () => {
+      translationServiceMock.getTranslation.and.callFake((someString) => of(someString));
+
+      const translationString = 'Hello World!';
+      expect(pipe.transform(translationString)).toBe(translationString);
+    });
+
+    it('should call getTranslation with the string argument when no replacements or yesOrNo are provided', () => {
+      const str = 'Hello world!';
+      const obs = of('Translated string');
+      translationServiceMock.getTranslation.and.returnValue(obs);
+
+      pipe.transform(str);
+
+      expect(translationServiceMock.getTranslation).toHaveBeenCalledWith(str);
+    });
+
+    it('should call getTranslationWithReplacements with the string and replacements arguments when replacements are provided', () => {
+      const str = 'Hello world!';
+      const replacements = { name: 'John' };
+      const obs = of('Translated string');
+      translationServiceMock.getTranslationWithReplacements.and.returnValue(obs);
+
+      pipe.transform(str, replacements);
+
+      expect(translationServiceMock.getTranslationWithReplacements).toHaveBeenCalledWith(str, replacements);
+    });
+
+    it('should call getYesTranslationReplacement with the string and yesOrNo argument when yesOrNo is "yes"', () => {
+      const str = 'Are you sure?';
+      const yesOrNo = 'yes';
+      const obs = of('Translated string');
+      translationServiceMock.getYesTranslationReplacement.and.returnValue(obs);
+
+      pipe.transform(str, null, yesOrNo);
+
+      expect(translationServiceMock.getYesTranslationReplacement).toHaveBeenCalledWith(str, yesOrNo);
+    });
+
+    it('should call getNoTranslationReplacement with the string and yesOrNo argument when yesOrNo is "no"', () => {
+      const str = 'Are you sure?';
+      const yesOrNo = 'no';
+      const obs = of('Translated string');
+      translationServiceMock.getNoTranslationReplacement.and.returnValue(obs);
+
+      pipe.transform(str, null, yesOrNo);
+
+      expect(translationServiceMock.getNoTranslationReplacement).toHaveBeenCalledWith(str, yesOrNo);
+    });
   });
 });
